97512c19358ef408ed20afde1cccce72381dc90f
Gabor Heja <gheja@m002560p>
2018-08-20 13:59:43 +0200

    lib: Added distance() to calculate the distance between two points

--- src/lib.js
+++ src/lib.js
@@ -64,6 +64,11 @@ function _getArrayKeys(a, keys)
 	return result;
 }
 
+function distance(x1, y1, x2, y2)
+{
+	return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+}
+
 function bindEvent(obj, event, callback)
 {
 	if (obj.addEventListener)
