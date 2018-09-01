ae92965cbf55c45443b0e64c5705aa8a5bba1c8e
Gabor Heja <gheja@m002560p>
2018-09-02 01:58:32 +0200

    GameObjectObstacle: simplified object creation

--- src/game_object_obstacle.js
+++ src/game_object_obstacle.js
@@ -11,28 +11,9 @@ class GameObjectObstacle extends GameObject
 		this.radius = r;
 		this.distance = 0;
 		this.paused = false;
-		
 		this.blockDown = a == OBJ_OBSTACLE_LOWER || a == OBJ_OBSTACLE_FULL;
 		this.blockUp = a == OBJ_OBSTACLE_UPPER || a == OBJ_OBSTACLE_FULL;
-		
-		switch (a)
-		{
-			case OBJ_OBSTACLE_FULL:
-				this.gfxObject = _gfx.placeObject(OBJ_OBSTACLE_FULL, { x: x, y: 0, z: y }, {});
-			break;
-			
-			case OBJ_OBSTACLE_LOWER:
-				this.gfxObject = _gfx.placeObject(OBJ_OBSTACLE_LOWER, { x: x, y: 0, z: y }, {});
-			break;
-			
-			case OBJ_OBSTACLE_UPPER:
-				this.gfxObject = _gfx.placeObject(OBJ_OBSTACLE_UPPER, { x: x, y: 0, z: y }, {});
-			break;
-			
-			case OBJ_EDGE:
-				this.gfxObject = _gfx.placeObject(OBJ_EDGE, { x: x, y: 0, z: y }, {});
-			break;
-		}
+		this.gfxObject = _gfx.placeObject(a, { x: x, y: 0, z: y }, {});
 	}
 	
 	tick()
